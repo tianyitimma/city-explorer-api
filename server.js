e0e6d08dc0f3128112e31fe27603433ba86dfdf9
mtygit <tim.ma0118@gmail.com>
2021-07-22 22:35:01 -0700

    build the server

--- server.js
+++ server.js
@@ -5,17 +5,16 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const cors = require('cors');
-const weather = require('./data/weather.json');
 
+const axios = require('axios');
 // config
 dotenv.config();
 app.use(cors());
-// app.use(express.json());
+app.use(express.json());
 
 //setup the port constant
 const PORT = process.env.PORT || 3333;
 
-//const API = `http://localhost:${PORT}?lat=${lat}&lon=${lon}&city=${q}`;
 
 // build the server page
 
@@ -24,26 +23,58 @@ app.listen(PORT, () =>{
 });
 
 
-app.get('/weather', (req, res) =>{
+
+app.get('/weather', (req, res) => {
   let lat = req.query.lat;
   let lon = req.query.lon;
-  let q = req.query.city;
 
+  const API = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
 
-  let cityWeather = weather.find(item => (
-    item.city_name.toLowerCase() === q.toLowerCase() &&
-    parseFloat(parseFloat(item.lon).toFixed(2)) === parseFloat(parseFloat(lon).toFixed(2)) &&
-    parseFloat(parseFloat(item.lat).toFixed(2)) === parseFloat(parseFloat(lat).toFixed(2))
-  ));
-  let dailyForecast = cityWeather.data.map(data => new Forecast(data));
-  res.send(dailyForecast);
-
+  axios.get(API)
+    .then(response => {
+      let dailyForecast = response.data.data.map(data => new Forecast(data));
+      res.send(dailyForecast);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('Sorry, something went wrong');
+    });
 
 });
 
 class Forecast {
   constructor(data) {
-    this.description = `Low of ${data.low_temp}, high of ${data.max_temp} with ${data.weather.description}`;
+    this.description = `Low of ${data.low_temp}, high of ${data.high_temp} with ${data.weather.description}`;
     this.date = data.datetime;
+  }
+}
+
+app.get('/movies', (req, res) =>{
+  let city = req.query.city;
+  const API = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}`;
+
+  axios.get(API)
+    .then(response => {
+      let movies = response.data.results.map(data => new Movies(data));
+      res.send(movies);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('Sorry, something went wrong');
+    });
+
+
+
+});
+
+class Movies {
+  constructor(data){
+    this.title = data.title,
+    this.overview = data.overview,
+    this.average_votes = data.vote_average,
+    this.total_votes = data.vote_count,
+    this.image_url = data.poster_path,
+    this.popularity = data.popularity,
+    this.released_on = data.release_date;
   }
 }
